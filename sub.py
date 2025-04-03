072d1e9482e2d26ebf422c50ab3ed3c5b55aae8f
Kesavk1 <93379619+Kesavk1@users.noreply.github.com>
2025-04-03 12:44:09 +0100

    Create sub.py

---
+++ sub.py
@@ -0,0 +1,4 @@
+submission_df = pd.read_csv('/kaggle/working/submission.csv')
+print("Overview of the DataFrame:")
+print(submission_df.shape)  # Print the shape (rows, columns)
+print(submission_df.head())  # Display the first 5 rows
